46fcd67608cad22153ef514c096ca12e6e25bedd
shyam <shyamsankar3100@gmail.com>
2024-07-27 19:13:03 +0530

    Remove Un Wanted

--- BULB/page2.js
+++
@@ -1,8 +0,0 @@
-function on(){
-    document.getElementById('img').src="lightbulb-g3469abdc8_1920.jpg"
-
-}
-function off(){
-    document.getElementById('img').src="lightbulb-g3469abdc8_ bulb off.jpg"
-    
-}
